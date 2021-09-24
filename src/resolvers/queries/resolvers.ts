563d31c1052d067a518462dc04914c6d097c3873
Said Torres <sesaid17@hotmail.com>
2021-09-24 11:34:23 -0500

    Conected to database and console query

--- src/resolvers/queries/resolvers.ts
+++ src/resolvers/queries/resolvers.ts
@@ -1,5 +1,13 @@
 import { GraphQLJSON } from "graphql-type-json";
 import * as data from '../../questions.json'
+import MariaDB from 'mariadb'
+
+const database = MariaDB.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  connectionLimit: 50
+})
 
 const resolvers = {
   JSON: GraphQLJSON,
@@ -10,6 +18,10 @@ const resolvers = {
   },
   Mutation: {
     createQuestion: async (parent: any, { texto }: { texto: string }): Promise<String> => {
+      const dataBaseConection = await database.getConnection()
+      await dataBaseConection.query("use questions_component")
+      const response = await dataBaseConection.query("SELECT * FROM answer") as any[]
+      console.log(response.pop())
       let defaultValue = "Default value"
       if (texto) defaultValue = texto
       return defaultValue
