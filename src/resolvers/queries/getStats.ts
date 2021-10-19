69ec77344572803c716b4cf99532248fc3d44b4f
Said Torres <sesaid17@hotmail.com>
2021-10-19 17:58:49 -0500

    getStats now can return 4 diferent values

--- src/resolvers/queries/getStats.ts
+++ src/resolvers/queries/getStats.ts
@@ -3,11 +3,18 @@ import { Connection } from "typeorm";
 import { Answer } from "../../entities/answer";
 import { Posted_Answer } from "../../entities/posted_answer";
 import { Question } from "../../entities/question";
+import { Respondent } from "../../entities/respondent";
 
 @ObjectType()
 class GetStatsPayload {
   @Field(type => Int)
+  monthlyAverageScore: number
+  @Field(type => Int)
   averageScore: number
+  @Field(type => Int)
+  questionsAmount: number
+  @Field(type => Int)
+  respondentsAmount: number
 }
 
 @Resolver()
@@ -16,7 +23,11 @@ export class GetStatsQuery {
   async getStats(
     @Ctx() connection: Connection
   ): Promise<GetStatsPayload> {
-    const postedAnswers = await connection.manager.find(Posted_Answer, {relations: ["answer"]})
+    const postedAnswers = await connection.manager.find(Posted_Answer, { relations: ["answer"] })
+    const respondentsAmount = await connection.manager.count(Respondent)
+    const questionsAmount = await connection.manager.count(Question)
+    let monthlyScores: number[] = [];
+    const today = new Date()
 
     let scores = await Promise.all(
       postedAnswers.map(async (postedAnswer) => {
@@ -24,6 +35,12 @@ export class GetStatsQuery {
         if (typeof answer.value === 'number') {
           const [answers, numberOfAnswersInQuestion] = await connection.manager.findAndCount(Answer, { where: { question: answer.question } })
           const score = answer.value / numberOfAnswersInQuestion
+          const timeDiff = today.getTime() - postedAnswer.createdAt.getTime()
+          const daysDiff = timeDiff / (1000 * 3600 * 24)
+          if(daysDiff <= 30) {
+            monthlyScores.push(score)
+          }
+
           return score
         }
         return -1
@@ -32,14 +49,23 @@ export class GetStatsQuery {
 
     scores = scores.filter(score => score >= 0)
 
-    let avgScore = 0;
+    let averageScore = 0;
     for (const score of scores) {
-      avgScore += score
+      averageScore += score
     }
-    avgScore = (avgScore / scores.length) * 100
+    
+    let monthlyAverageScore = 0;
+    for (const monthlyScore of monthlyScores) {
+      monthlyAverageScore += monthlyScore
+    }
 
+    averageScore = (averageScore / scores.length) * 100
+    monthlyAverageScore = (monthlyAverageScore / monthlyScores.length) * 100
     return {
-      averageScore: avgScore
+      monthlyAverageScore,
+      averageScore,
+      questionsAmount,
+      respondentsAmount
     }
   }
 }
